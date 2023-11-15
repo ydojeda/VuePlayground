09f7bdbf59c650f4d9b5bb8efa3f91c8baf5fd27
ydojeda <yna.ojeda@asknice.ly>
2023-11-15 15:53:58 +1300

    set up blank views/pages for exercises

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Exercise1 from '../views/Exercise1.vue'
+import Exercise2 from '@/views/Exercise2.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'ex1',
+      component: Exercise1
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/ex2',
+      name: 'ex2',
+      component: Exercise2
     }
   ]
 })
---
+++ src/views/Exercise1.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <h1>Vue Fundamentals</h1>
+  </main>
+</template>
