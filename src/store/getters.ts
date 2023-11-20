8272acf8480df8facd9213018df1cc0c7f6e02e5
ydojeda <yna.ojeda@asknice.ly>
2023-11-21 08:44:55 +1300

    store | add allUsers getter

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -2,6 +2,8 @@ import type { OverallState } from './types'
 
 export const usersById = (state: OverallState) => state.users
 
+export const allUsers = (state: OverallState) => Object.values(state.users)
+
 export const postsByUser = (state: OverallState) => state.posts
 
 export const reactionsByUser = (state: OverallState) => state.posts
