0473b7ef564a4c0cc143f40a12231b5b700f86fb
ydojeda <yna.ojeda@asknice.ly>
2023-11-21 13:30:06 +1300

    icons | add regular font awesome icons to library

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import App from './App.vue'
 import router from './router'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import store from './store'
 
-library.add(fas)
+library.add(fas, far)
 
 const app = createApp(App)
 app.use(store)
