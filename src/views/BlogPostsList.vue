e3be2e368ea3f12da32464c2da0a920a4ba1d1b4
ydojeda <yna.ojeda@asknice.ly>
2023-11-21 16:16:20 +1300

    Blog Post List | display blog posts

--- src/views/BlogPostsList.vue
+++ src/views/BlogPostsList.vue
@@ -1,15 +1,85 @@
 <template>
-  <div class="about">
-    <h1>Vue Component Fundamentals</h1>
+  <div class="page-container">
+    <div class="header-container">
+      <UserSwitcher :current-user="currentUser" :user-id="currentUser.userId" />
+      <button v-on:click="goToNewForm" class="add-new-post-button">New Post</button>
+    </div>
+    <div class="content-container">
+      <h1 class="title-text">Blog posts</h1>
+      <div class="posts-list-container">
+        <BlogPostCard
+          v-for="post in allPosts"
+          :post="post"
+          :current-user-id="currentUser.userId"
+          :key="post.postId"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+<script setup lang="ts">
+import { useStore } from 'vuex'
+import { computed } from 'vue'
+import UserSwitcher from '@/components/UserSwitcher.vue'
+import BlogPostCard from '@/components/BlogPostCard.vue'
+import { useRouter } from 'vue-router'
+
+const store = useStore()
+const currentUser = computed(() => store.getters.usersById['1'])
+const allPosts = computed(() => store.getters.allPosts)
+
+const router = useRouter()
+const goToNewForm = () => {
+  console.log('attempting to go to new form')
+  router.push({
+    name: 'Blog post form'
+  })
+}
+</script>
+
+<style lang="less" scoped>
+.page-container {
+  margin: 0 10px;
+}
+.page-content-base {
+  background-color: white;
+  border-radius: 3px;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+  padding: 20px 35px;
+}
+.content-container {
+  .page-content-base();
+}
+.header-container {
+  .page-content-base();
+  position: sticky;
+  left: 0;
+  right: 10px;
+  top: 0;
+  z-index: 10;
+  display: flex;
+  justify-content: space-between;
+  margin: 10px 0;
+}
+.title-text {
+  margin: 20px 0;
+}
+.posts-list-container {
+  display: flex;
+  flex-direction: column;
+}
+.add-new-post-button {
+  line-height: 32px;
+  font-size: 14px;
+  color: white;
+  padding: 0 16px;
+  border: rgb(36, 137, 218) 1px solid;
+  border-radius: 4px;
+  background-color: rgb(43, 155, 244);
+  cursor: pointer;
+  &:hover {
+    background-color: #2489da;
   }
 }
 </style>
